126958c686c3c32ce4c13ab88c296809c54cb533
uzma29 <48977018+uzma29@users.noreply.github.com>
2019-03-28 14:30:22 +0530

    Update test_hello.c

--- test_hello.c
+++ test_hello.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 #include <assert.h>
 #include <limits.h>
-#include "simple-math.h"
-
-
-
 void test_max(void)
 {
   printf ("Test max using assert:      ");
   assert (max(13,14)==14);
   assert (max(14,13)==14);
   /* assert (max(INT_MAX,14)==INT_MAX); */
-  /* assert (max(INT_MAX,INT_MIN)==INT_MAX); */
-  /* assert (max(-23000,INT_MIN)==-23000); */
+  /* asserax(INT_MAX,INT_MIN)==INT_MAX); */
+  /* assert (max(-23000,INt (mT_MIN)==-23000); */
   printf (" OK\n");
 }
