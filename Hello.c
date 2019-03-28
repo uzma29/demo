3656c6ebd7df16096683760d58d7ff5530ebe351
uzma29 <48977018+uzma29@users.noreply.github.com>
2019-03-28 12:30:05 +0530

    Update and rename test.c to Hello.c

---
+++ Hello.c
@@ -0,0 +1,17 @@
+ #include <stdio.h>
+#include <limits.h>
+#include <math.h>
+
+#include "simple-math.h"
+
+int max(int a, int b)
+{
+  if (a>b)
+    {
+      return a;
+    }
+  return b;
+}
+
+
+
--- test.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-main()
-{
-  int a=20,b=30;
-  int sum=a+b;
-  
-printf("sum=%d",sum);
-}
