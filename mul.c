34bb92c76461ef8dc205549431f45f09a5bec40b
uzma29 <48977018+uzma29@users.noreply.github.com>
2019-03-29 09:11:57 +0530

    Create mul.c

---
+++ mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <limits.h>
+#include <math.h>
+#include <assert.h>
+#include "simple-math.h"
+
+void mul(int a, int b)
+{
+  printf("Input two integers to multiply\n");
+  scanf("%d%d", &a, &b);
+
+  assert(a*b<100000);
+
+
+  printf("%d * %d = %d\n", a, b, a*b);
+
+
+}
+
+
