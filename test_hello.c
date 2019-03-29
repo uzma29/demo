3a181a81c7ef93acfafa2ab10d80b38dfc087111
uzma29 <48977018+uzma29@users.noreply.github.com>
2019-03-29 09:09:33 +0530

    Update test_hello.c

--- test_hello.c
+++ test_hello.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
-#include <assert.h>
 #include <limits.h>
-#include "simple_header.h"
-void test_max(void)
+#include <math.h>
+#include <assert.h>
+#include "simple-math.h"
+
+void division(int a, int b)
 {
-  printf ("Test max using assert:      ");
-  assert (max(13,14)==14);
-  assert (max(14,13)==14);
-  /* assert (max(INT_MAX,14)==INT_MAX); */
-  /* asserax(INT_MAX,INT_MIN)==INT_MAX); */
-  /* assert (max(-23000,INt (mT_MIN)==-23000); */
-  printf (" OK\n");
+  printf("Input two integers to divide\n");
+  scanf("%d%d", &a, &b);
+
+  assert(b != 0);
+  printf("%d/%d = %.2f\n", a, b, a/(float)b);
+
 }
+
+
